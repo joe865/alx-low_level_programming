0cfc35da889aca61fe12664044afa5b47277c767
Your Name <you@example.com>
2022-10-13 14:23:09 -0700

    great

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - the first function
+*return: Meant to return 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
