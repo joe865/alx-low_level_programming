196c76235ef5ca05694aa084111a45f9c69f3999
Your Name <you@example.com>
2022-10-18 16:01:31 -0700

    print alpha x10

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_sign - print signs of number 
+ * @s: A charcter
+ * Return: 1 only when n is greater than 0, when n is 0 return 0 ,  when less than 0 
+ * return -1
+ */
+int _isalpha(int s)
+{
+	if (s > 0)
+		return(+, 1)
+	if (s = 0)
+		return (0, 0);
+	else
+		return (-, -1);
+}
