b2431e7c25f8b78e080f3b38d075e2a0d562e7dc
Your Name <you@example.com>
2022-10-18 12:36:14 -0700

    task 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,18 @@
  * Return: Returns 0
  */
 
-int main(void)
+void print_alphabet(void)
 {
 	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-		putchar (ch);
+	putchar(ch);
 	}
+}
 
+int main(void)
+{
+	print_alphabet();
 	return (0);
 }
