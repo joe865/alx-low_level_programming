714d0f4cccb63eaf4dc4bffb03bd0a05d658dc61
Your Name <you@example.com>
2022-10-18 18:49:27 -0700

    message

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Add two integers by function.
+ * @i: First integer variable to add.
+ * @i: First integer variable to add.
+ * Return: return with summation.
+ */
+int add(int i, int l)
+{
+	int n;
+	 n = i + l;
+	return (n);
+}
