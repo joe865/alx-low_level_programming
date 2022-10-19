d3ae8f2915faf1006d9da9207cb07aa4b92ccf6c
Your Name <you@example.com>
2022-10-18 19:16:19 -0700

    alx

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,29 +1,23 @@
 #include <stdio.h>
-
 /**
- * main - print sum of even Fionacci sequence up to 4,000,000
+ * main - main block
+ * Description: computes and prints even  number < 4,000,000
+ * 5 below 1024 (excluded), followed by a new line
  * Return: 0
  */
-
 int main(void)
 {
-	int sum_of_evens = 0;
-	int a;
-	int b;
-	int sum = 1;
+	int a = 0, b = 1, next = 0;
+	int sum = 0;
 
-	a = 1;
-	b = 1;
-
-	while (b < 4000000)
+	while (next < 4000000)
 	{
-		sum = a + b;
+		next = a + b;
 		a = b;
-		b = sum;
-		if ((sum <= 4000000) && (sum % 2 == 0))
-			sum_of_evens += sum;
+		b = next;
+		if (next % 2 == 0)
+			sum += next;
 	}
-	printf("%d\n", sum_of_evens);
-
+	printf("%i\n", sum);
 	return (0);
 }
