63546708f0cce15520d6776eadfa4a9912b0f409
Your Name <you@example.com>
2022-10-24 13:18:53 -0700

    Pointer

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,11 +6,17 @@
  */
 void print_rev(char *s)
 {
-	int i = 0;
+	int i;
+	char rv = s[0];
+	int c = 0;
 
-	while (s[i] != '\0')
-		i++;
-	for (i = i - 1; i >= 0; i--)
-		_put char(s[i]);
-	_putchar('\n');
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
+	}
 }
