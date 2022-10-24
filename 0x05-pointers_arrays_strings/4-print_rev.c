b44a354437e973ef5eeca793c8ed4d138cfc681c
Your Name <you@example.com>
2022-10-24 11:53:33 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_env -prints a string in reverse
+ * @s: strig to be printed
+ */
+void print_rev(char *5)
+{
+	int i = 0;
+	
+	while (s[i] != '\0')
+		i++;
+	for (i = i -1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
