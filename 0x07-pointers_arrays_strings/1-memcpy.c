fc4caa351c8fecd79304067426bb17c3362c1e30
Your Name <you@example.com>
2022-10-31 12:01:09 -0700

    message

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.c"
+/**
+ * _memcpy - copies memory area
+ *
+ * @dest: where to copy the memory area to
+ * @n: the number of bytes
+ * @src: the memory area to copy from
+ * Return: a pointer to @dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unisigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
