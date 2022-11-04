f76891ea9cbd84b88df26982d4c68fae05dd0987
Your Name <you@example.com>
2022-11-04 15:36:42 -0700

    argc

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,24 @@
+#include <stdin.h>
+#include "main.h"
+
+/**
+ * main - print the name of the program
+ * @argc: count arguements
+ * @argv: Arguements
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0; 
+	if (argc > 0)
+	{
+		while (count < argc)
+		{
+			printf("%s\n", argv[count]);
+			count++;
+		}
+	}
+	return (0);
+}
