8799cdfff505b8fdf1662b8f2017e7e7b14b8ca8
Your Name <you@example.com>
2022-11-04 17:15:09 -0700

    argc

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,21 +10,29 @@
 
 int main(int argc, char *argv[])
 {
-	int position, total, change, aux;
+	int num, j, result = 0;
 	int coins[] = {25, 10, 5, 2, 1};
-
-	position = total = change = aux = 0;
 
 	if (argc != 2)
 	{
-		printf("Error\n");
-		return (0);
+		printf("%s\n", "Error");
+		return (1);
 	}
-	total = atoi(argv[1]);
 
-	if (total <= 0)
+	num = atoi(argv[1]);
+	if (num < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
+	for (j = 0; j < 5 && num >= 0; j++)
+	{
+		while (num >= coins[j])
+		{
+			num -= coins[j];
+			result++;
+		}
+	}
+	printf("%d\n", result);
+	return (0);
 }
